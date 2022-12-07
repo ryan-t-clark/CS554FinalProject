bb079b8a68d3ddd5c23c55048bd4ceb439dbe07d
Roshan <rnarma@stevens.edu>
2022-12-06 20:41:56 -0500

    rough Front-end for login/signup

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -1,13 +1,50 @@
 import React, { FC } from 'react';
+import axios from 'axios';
 
 interface LoginProps {};
 
 const Login: FC<LoginProps> = () => {
+
+
+
+    async function login(username:string,password:string){
+        try{ const response = await axios.post("http://localhost:3008/users/login",{
+            "username":username,
+            "password":password
+        })
+        console.log(response);
+        } catch (error){
+            console.log(error);
+            console.log("invalid login");
+        } 
+
+    }
     
     
     return (
         <div>
             <h2>Log in</h2>
+            <form onSubmit={(e: React.SyntheticEvent) => {
+                e.preventDefault();
+                const target = e.target as typeof e.target & {
+                    username: {value:string};
+                    password: {value:string};
+                }
+                const username = target.username.value;
+                const password = target.password.value;
+                login(username,password);
+            }
+            }>
+                <label>
+                username:
+                <input type="text" name="username"></input>
+                </label>
+                <label>
+                password:
+                <input type="text" name="password"></input>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
--- client/src/components/pages/Signup.tsx
+++ client/src/components/pages/Signup.tsx
@@ -1,13 +1,49 @@
 import React, { FC } from 'react';
+import axios from 'axios';
 
 interface SignupProps {};
 
 const Signup: FC<SignupProps> = () => {
     
     
+    async function signup(username:string,password:string){
+        try {const response = await axios.post("http://localhost:3008/users/signup",{
+            "username":username,
+            "password":password
+        })
+        console.log(response);
+        } catch (error){
+            console.log(error);
+            console.log("invalid login");
+        } 
+
+    }
+    
+    
     return (
         <div>
-            <h2>Signup</h2>
+            <h2>Log in</h2>
+            <form onSubmit={(e: React.SyntheticEvent) => {
+                e.preventDefault();
+                const target = e.target as typeof e.target & {
+                    username: {value:string};
+                    password: {value:string};
+                }
+                const username = target.username.value;
+                const password = target.password.value;
+                signup(username,password);
+            }
+            }>
+                <label>
+                username:
+                <input type="text" name="username"></input>
+                </label>
+                <label>
+                password:
+                <input type="text" name="password"></input>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
