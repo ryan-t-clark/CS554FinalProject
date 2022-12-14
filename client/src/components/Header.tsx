194bbf59021b054ebc670cb3a679d06520dede31
Enrique Valentino <enriquefvalentino@gmail.com>
2022-12-14 15:50:04 -0500

    header and home page material ui conversion

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,44 +6,19 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 interface HeaderProps {};
 
 const Header: FC<HeaderProps> = () => {
-    const [currentRoute, setCurrentRoute] = useState('');
-    const setRoute = ((route:string) => {
-        setCurrentRoute(route);
-    })
+
+    const logout = () => {
+        Cookies.remove('username'); 
+        Cookies.remove('userId'); 
+        Cookies.remove('admin');
+    }
+    
     return (
-        // I might make this responsive like we did with bootstrap header, but thatll be for later
-        // <Box position='fixed' overflow='hidden' bg='#393b3d' w='100%' h='12%'>
-        //     <Center marginBottom='2px'>
-        //         <Heading p='10px' color='white' as='h1'>NFL Game Picker</Heading>
-        //     </Center>
-        //     <Center>
-        //         <Link as={RouterLink} to={`/`} color= {currentRoute==='home' ? 'white' : '#545e6f'} className={currentRoute === 'home' ? 'selected-link' : 'nav-link'} >
-        //             Home
-        //         </Link>
-        //         <Link as={RouterLink} to={`/week`} color= {currentRoute==='week' ? 'white' : '#545e6f'} className={currentRoute === 'week' ? 'selected-link' : 'nav-link'} >
-        //             Week
-        //         </Link>
-        //         <Link as={RouterLink} to={`/picks`} color= {currentRoute==='picks' ? 'white' : '#545e6f'} className={currentRoute === 'picks' ? 'selected-link' : 'nav-link'} >
-        //             Picks
-        //         </Link>
-        //         <Link as={RouterLink} to={`/leaderboards`} color= {currentRoute==='leaderboards' ? 'white' : '#545e6f'} className={currentRoute === 'leaderboards' ? 'selected-link' : 'nav-link'} >
-        //             Leaderboards
-        //         </Link>
-        //         <Link as={RouterLink} to={`/login`} color= {currentRoute==='login' ? 'white' : '#545e6f'} className={currentRoute === 'login' ? 'selected-link' : 'nav-link'} >
-        //             Login
-        //         </Link>
-        //         <Link as={RouterLink} to={`/signup`} color= {currentRoute==='signup' ? 'white' : '#545e6f'} className={currentRoute === 'signup' ? 'selected-link' : 'nav-link'} >
-        //             Signup
-        //         </Link>
-        //         <Link as={RouterLink} to={`/logout`} color= {currentRoute==='logout' ? 'white' : '#545e6f'} className={currentRoute === 'logout' ? 'selected-link' : 'nav-link'} onClick={() => { Cookies.remove('userId'); Cookies.remove('username'); Cookies.remove('admin');}}>
-        //             Logout
-        //         </Link>
-        //     </Center>
-        // </Box>
         <AppBar style={{ background: '#393b3d' }}>
                 <Typography component="h1" variant="h4" padding="10px" align="center">
                         NFL Game Picker
@@ -93,13 +68,12 @@ const Header: FC<HeaderProps> = () => {
                                 background: isActive ? '#1310c0' : '#f0f0f0',
                             })}>Sign up
                         </NavLink>
-                        <NavLink
-                            to="/logout"
-                            style={({ isActive }) => ({
-                                color: isActive ? '#fff' : '#545e6f',
-                                background: isActive ? '#1310c0' : '#f0f0f0',
-                            })}>Log out
-                        </NavLink>
+                        <Button
+                            onClick={logout}
+                            variant='contained'
+                        >
+                            Log out
+                        </Button>
                     </div>
                 </Toolbar>
         </AppBar>
--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,175 +1,67 @@
 import React, { FC } from 'react';
+import { Box, Typography } from '@mui/material';
 
 interface HomeProps {};
 
 const Home: FC<HomeProps> = () => {
     return (
-        <div>Changing</div>
-        // <Center paddingBottom='100px'>
-        //     <Box w='50%'>
-        //         <Center>
-        //             <Heading as='h2' size='xl'>Welcome to NFL Game Picker!</Heading>
-        //         </Center>
-        //         <Spacer />
-        //         <br></br>
-        //         <Box>
-        //             <Heading as='h3' size='md'>NFL Game Picker</Heading>
-        //             <br></br>
-        //             <Text>
-        //                 NFL Game Picker is a competitive site that will allow users to compete against one another in guessing the outcomes
-        //                 of upcoming NFL games during the regular season. Players will be able to compete for points and bragging rights
-        //                 in this heated competition.
-        //             </Text>
-        //         </Box>
-        //         <br></br>
-        //         <Box>
-        //             <Heading as='h3' size='md'>Picks and Scoring</Heading>
-        //             <br></br>
-        //             <Text>
-        //                 Each week a schedule of games will be released for NFL games. In most cases these games will be released on Tuesday, 
-        //                 and all players will receive an email notification alerting them to the release of the upcoming week's schedule.
-        //             </Text>
-        //             <br></br>
-        //         </Box>
-        //         <br></br>
-        //         <Box>
-        //             <Heading as='h3' size='md'>Registration</Heading>
-        //             <br></br>
-        //             <Text> To register, just simply create an account and get picking! </Text>
-        //         </Box>
-        //         <br></br>
-        //         <Box>
-        //             <Heading as='h3' size='md'>Rules</Heading>
-        //             <br></br>
-        //             <Text>
-        //                 Each week players will make their picks. Although a player is not required to make all picks, there is no penalty for a loss so it is in a player's best interest to always make their picks.
-        //             </Text>
-        //             <br></br>
-        //             <Text>
-        //                 A player is not required to submit all picks at once. As long as a pick is made before a game is started a player is free to submit picks at any time.
-        //             </Text>
-        //             <br></br>
-        //             <Text>
-        //                 Each pick made will be given a "confidence weight" from 10 down to 1. For each correct selection a player will receive that many points for that game. Once a "confidence weight" is set for a pick, 
-        //                 that weight is no longer available for any remaining picks. In other words, a player can only choose 1 pick to have a value of 10, 1 pick to have a value of 9 and so on.
-        //             </Text>
-        //                 <br></br>
-        //             <Text>A pick may not be made on a game that has already begun.</Text>
-        //         </Box>
-        //         <br></br>
-        //         <Box>
-        //             <Heading as='h3' size='md'>Weekly Scedule</Heading>
-        //             <br></br>
-        //             <Text> There are 18 weeks in the competition </Text>
-        //             <br></br>
-        //             <Text> We can edit this stuff later... </Text>
-        //             <TableContainer maxWidth='100%'>
-        //                 <Table variant='striped'>
-        //                     <Thead>
-        //                         <Tr>
-        //                             <Th>Week</Th>
-        //                             <Th>Week Start</Th>
-        //                             <Th>Week End</Th>
-        //                         </Tr>
-        //                     </Thead>
-        //                     <Tbody>
-        //                         <Tr>
-        //                             <Td>Week 1</Td>
-        //                             <Td>Week 1 Start</Td>
-        //                             <Td>Week 1 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 2</Td>
-        //                             <Td>Week 2 Start</Td>
-        //                             <Td>Week 2 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 3</Td>
-        //                             <Td>Week 3 Start</Td>
-        //                             <Td>Week 3 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 4</Td>
-        //                             <Td>Week 4 Start</Td>
-        //                             <Td>Week 4 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 5</Td>
-        //                             <Td>Week 5 Start</Td>
-        //                             <Td>Week 5 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 6</Td>
-        //                             <Td>Week 6 Start</Td>
-        //                             <Td>Week 6 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 7</Td>
-        //                             <Td>Week 7 Start</Td>
-        //                             <Td>Week 7 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 8</Td>
-        //                             <Td>Week 8 Start</Td>
-        //                             <Td>Week 8 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 9</Td>
-        //                             <Td>Week 9 Start</Td>
-        //                             <Td>Week 9 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 10</Td>
-        //                             <Td>Week 10 Start</Td>
-        //                             <Td>Week 10 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 11</Td>
-        //                             <Td>Week 11 Start</Td>
-        //                             <Td>Week 11 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 12</Td>
-        //                             <Td>Week 12 Start</Td>
-        //                             <Td>Week 12 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 13</Td>
-        //                             <Td>Week 13 Start</Td>
-        //                             <Td>Week 13 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 14</Td>
-        //                             <Td>Week 14 Start</Td>
-        //                             <Td>Week 14 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 15</Td>
-        //                             <Td>Week 15 Start</Td>
-        //                             <Td>Week 15 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 16</Td>
-        //                             <Td>Week 16 Start</Td>
-        //                             <Td>Week 16 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 17</Td>
-        //                             <Td>Week 17 Start</Td>
-        //                             <Td>Week 17 End</Td>
-        //                         </Tr>
-        //                         <Tr>
-        //                             <Td>Week 18</Td>
-        //                             <Td>Week 18 Start</Td>
-        //                             <Td>Week 18 End</Td>
-        //                         </Tr> 
-        //                     </Tbody>
-        //                 </Table>
-        //             </TableContainer>
-        //         </Box>
-        //         <br></br>
-        //     </Box>
-        // </Center>
+        <div >
+            <center>
+                <Typography variant='h2' gutterBottom>
+                    Welcome to NFL Game Picker!
+                </Typography>                
+            </center>
+
+            <div>
+                <Box sx={{ 
+                    display: 'block',
+	                marginLeft: 'auto',
+	                marginRight: 'auto',
+	                width: '50%',
+                }}>
+                    <Typography variant='h4' gutterBottom>
+                        NFL Game Picker
+                    </Typography>
+                    <Typography variant='body1' gutterBottom>
+                        NFL Game Picker is a competitive site that will allow users to compete against one another in guessing the outcomes
+                        of upcoming NFL games during the regular season. Players will be able to compete for points and bragging rights
+                        in this heated competition.
+                    </Typography>
+                    <br />
+                    <Typography variant='h4' gutterBottom>
+                        Picks and Scoring
+                    </Typography>
+                    <Typography variant='body1' gutterBottom>
+                        Each week a schedule of games will be released for NFL games. In most cases these games will be released on Tuesday, 
+                        and all players will receive an email notification alerting them to the release of the upcoming week's schedule.
+                    </Typography>
+                    <br />
+                    <Typography variant='h4' gutterBottom>
+                        Registration
+                    </Typography>
+                    <Typography variant='body1' gutterBottom>
+                        To register, just simply create an account and get picking!
+                    </Typography>
+                    <br />
+                    <Typography variant='h4' gutterBottom>
+                        Rules
+                    </Typography>
+                    <Typography variant='body1' gutterBottom>
+                        Each week players will make their picks. Although a player is not required to make all picks, there is no penalty for a loss so it is in a player's best interest to always make their picks.
+                        <br />
+                        <br />
+                        A player is not required to submit all picks at once. As long as a pick is made before a game is started a player is free to submit picks at any time.
+                        <br />
+                        <br />
+                        Each pick made will be given a "confidence weight" from 10 down to 1. For each correct selection a player will receive that many points for that game. Once a "confidence weight" is set for a pick, 
+                        that weight is no longer available for any remaining picks. In other words, a player can only choose 1 pick to have a value of 10, 1 pick to have a value of 9 and so on.
+                        <br />
+                        <br />
+                        A pick may not be made on a game that has already begun.
+                    </Typography>
+                </Box>
+            </div>
+        </div>
     )
 }
 
