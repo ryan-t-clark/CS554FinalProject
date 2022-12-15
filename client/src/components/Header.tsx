4d6d22a8fa47f9590c56090acc717d7787448e12
Roshan <rnarma@stevens.edu>
2022-12-15 13:55:57 -0500

    header changes and logout

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React, { FC, useState, useEffect} from 'react';
-import { Navigate, NavLink, redirect } from 'react-router-dom';
+import React, { FC, useState, useEffect, useCallback} from 'react';
+import { Navigate, NavLink, redirect, useLocation } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 import AppBar from '@mui/material/AppBar';
@@ -17,6 +17,57 @@ const Header: FC<HeaderProps> = () => {
         Cookies.remove('userId'); 
         Cookies.remove('admin');
     }
+    const userId = Cookies.get('userId');
+    const { pathname } = useLocation();
+    var Picks,Profile,Logout,Signup,Login;
+    // var Profile;
+    // var Logout;
+    // var signup,login;
+    if(userId){
+        Picks = <NavLink
+                    to="/picks"
+                    style={({ isActive }) => ({
+                        color: isActive ? '#fff' : '#545e6f',
+                        background: isActive ? '#1310c0' : '#f0f0f0',
+                    })}>Make Picks
+                </NavLink>
+        Profile = <NavLink
+                    to="/profile"
+                    style={({ isActive }) => ({
+                        color: isActive ? '#fff' : '#545e6f',
+                        background: isActive ? '#1310c0' : '#f0f0f0',
+                    })}>Profile
+                </NavLink>
+        Logout = <NavLink
+                    to="/logout"
+                    style={({ isActive }) => ({
+                        color: isActive ? '#fff' : '#545e6f',
+                        background: isActive ? '#1310c0' : '#f0f0f0',
+                    })}>Logout
+                </NavLink>
+        // Logout = <Button
+        //             onClick={logout}
+        //             variant='contained'>
+        //             Log out
+        //         </Button>
+    } else {
+        Login = <NavLink
+                    to="/login"
+                    style={({ isActive }) => ({
+                        color: isActive ? '#fff' : '#545e6f',
+                        background: isActive ? '#1310c0' : '#f0f0f0',
+                    })}>Log in
+                </NavLink>
+        Signup = <NavLink
+                    to="/signup"
+                    style={({ isActive }) => ({
+                        color: isActive ? '#fff' : '#545e6f',
+                        background: isActive ? '#1310c0' : '#f0f0f0',
+                    })}>Sign up
+                </NavLink>
+    }
+    // console.log("here");
+    
     
     return (
         <AppBar style={{ background: '#393b3d' }}>
@@ -40,13 +91,7 @@ const Header: FC<HeaderProps> = () => {
                                 background: isActive ? '#1310c0' : '#f0f0f0',
                             })}>This Week
                         </NavLink>
-                        <NavLink
-                            to="/picks"
-                            style={({ isActive }) => ({
-                                color: isActive ? '#fff' : '#545e6f',
-                                background: isActive ? '#1310c0' : '#f0f0f0',
-                            })}>Make Picks
-                        </NavLink>
+                        {Picks}
                         <NavLink
                             to="/leaderboards"
                             style={({ isActive }) => ({
@@ -54,33 +99,10 @@ const Header: FC<HeaderProps> = () => {
                                 background: isActive ? '#1310c0' : '#f0f0f0',
                             })}>Leaderboards
                         </NavLink>
-                        <NavLink
-                            to="/login"
-                            style={({ isActive }) => ({
-                                color: isActive ? '#fff' : '#545e6f',
-                                background: isActive ? '#1310c0' : '#f0f0f0',
-                            })}>Log in
-                        </NavLink>
-                        <NavLink
-                            to="/signup"
-                            style={({ isActive }) => ({
-                                color: isActive ? '#fff' : '#545e6f',
-                                background: isActive ? '#1310c0' : '#f0f0f0',
-                            })}>Sign up
-                        </NavLink>
-                        <NavLink
-                            to="/profile"
-                            style={({ isActive }) => ({
-                                color: isActive ? '#fff' : '#545e6f',
-                                background: isActive ? '#1310c0' : '#f0f0f0',
-                            })}>Profile
-                        </NavLink>
-                        <Button
-                            onClick={logout}
-                            variant='contained'
-                        >
-                            Log out
-                        </Button>
+                        {Login}
+                        {Signup}
+                        {Profile}
+                        {Logout}
                     </div>
                 </Toolbar>
         </AppBar>
--- client/src/components/pages/Logout.tsx
+++ client/src/components/pages/Logout.tsx
@@ -1,12 +1,18 @@
 import React, { FC, useState } from 'react';
 import axios from 'axios';
+import Cookies from 'js-cookie';
+import { Navigate, Route, RouteProps } from 'react-router-dom';
+
 
 interface LogoutProps {};
 
 const Logout: FC<LogoutProps> = () => {
+    Cookies.remove('username'); 
+    Cookies.remove('userId'); 
+    Cookies.remove('admin');
 
     return (
-        <div>Test</div>
+        <Navigate to='/'/>
     )
 
 }
