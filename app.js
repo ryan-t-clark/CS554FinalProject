921e41ef13a638797424f3083a456df46d81d93f
Ryan Chin <ryanchin26@gmail.com>
2022-12-15 23:11:48 -0500

    return the client side html from server

--- app.js
+++ app.js
@@ -16,6 +16,21 @@ app.use(cors());
 
 configRoutes(app);
 
+// static file declaration
+if (process.env.NODE_ENV === 'production') {
+    // prod mode
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+  }
+  else {
+    app.use(express.static('client/public'))
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
+    })
+  }
+
 const port = process.env.PORT || 3008;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
