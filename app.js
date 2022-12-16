431c627cfb064a711c17eadfcada5b29ca4e18be
Ryan Chin <ryanchin26@gmail.com>
2022-12-16 00:38:05 -0500

    may have just went to wrong route

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ configRoutes(app);
 if (process.env.NODE_ENV === 'production') {
     // prod mode
     app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
   }
   else {
     app.use(express.static('client/public'))
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
     })
   }
