3b8af634e39f7ac41ba55a0d97796edd8b589b25
roshan <roshan@narma.com>
2022-12-20 01:39:21 -0500

    broken images :(

--- client/src/components/pages/Profile.tsx
+++ client/src/components/pages/Profile.tsx
@@ -3,6 +3,8 @@ import { Box, Button, Typography } from '@mui/material';
 import { setSelectionRange } from '@testing-library/user-event/dist/utils';
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import { saveAs } from 'file-saver';
+import baseUrl from '../../environment.js';
 
 // const gm = require('gm').subclass({imageMagick:true});
 
@@ -11,45 +13,87 @@ interface ProfileProps {};
 const Profile: FC<ProfileProps> = () => {
     // const fs = require('fs');
     // const gm = require('gm').subclass({imageMagick:true});
-    const [selectedImage, setSelectedImage] = useState('');
+    // const [selectedImage, setSelectedImage] = useState('');
+    // const t = new File(null as unknown[],"a");
+
+    const [selectedFile,setSelectedFile] = useState<File>();
+    const [loaded, setLoaded] = useState(0);
+
     const userId = Cookies.get('userId');
-    async function updateImage(event: React.ChangeEvent<HTMLInputElement>){
-        if (event.target.files && event.target.files[0]) {
-            let img = event.target.files[0];
-            console.log(img);
-            var x = URL.createObjectURL(img)
-
-            // gm()
-            setSelectedImage(x)
-            // var x = URL.createObjectURL(img)
-            // setSelectedImage(x)
-            console.log("here");
-            const formData = new FormData();
-            formData.append("image",event.target.files[0]);
-            // try{
-                // const link = process.env.NODE_ENV === 'production' ? 'http://127.0.0.1:8080/api' : 'http://localhost:3008/api'
-                // console.log(link);
-                // console.log(`${link}/users/updateImage/${userId}`);
+    // async function updateImage(event: React.ChangeEvent<HTMLInputElement>){
+    //     if (event.target.files && event.target.files[0]) {
+    //         let img = event.target.files[0];
+    //         console.log(img);
+    //         // const x = saveAs(img);
+    //         var x = URL.createObjectURL(img)
+    //         console.log(x);
+            
+    //         // gm()
+    //         // setSelectedImage(x)
+    //         // var x = URL.createObjectURL(img)
+    //         // setSelectedImage(x)
+    //         console.log("here");
+    //         // const formData = new FormData();
+    //         // formData.append("image",event.target.files[0]);
+    //         try{
+    //             const link = process.env.NODE_ENV === 'production' ? 'http://127.0.0.1:8080/api' : 'http://localhost:3008/api'
+    //             console.log(link);
+    //             console.log(`${link}/users/updateImage/${userId}`);
                 
-                // fetch(`${link}/users/updateImage/${userId}`,{
-                    // method:"POST" as string,
+    //             // fetch(`${link}/users/updateImage/${userId}`,{
+    //                 // method:"POST" as string,
 
-                    // body:formData, 
-                    // dataType: "json" as Object
-                // }) as RequestInit
-                // const response = await axios.post(`${link}/users/updateImage/${userId}`,{
-                //     "image":x
-                // });
-                // console.log(response);
-            // } catch (error) {
-                // console.log(error);
-                // console.log(""); 
-            // }
-            console.log("here 2");
+    //                 // body:formData, 
+    //                 // dataType: "json" as Object
+    //             // }) as RequestInit
+    //             const response = await axios.post(`${link}/users/updateImage/${userId}`,{
+    //                 "image":x
+    //             });
+    //             console.log(response);
+    //         } catch (error) {
+    //             console.log(error);
+    //             console.log(""); 
+    //         }
+    //         console.log("here 2");
              
-            // console.log(URL.createObjectURL(img));
-            // console.log(selectedImage);
-          }
+    //         // console.log(URL.createObjectURL(img));
+    //         // console.log(selectedImage);
+    //       }
+    // }
+    
+    async function handleselectedFile(event: React.ChangeEvent<HTMLInputElement>){
+        if (event.target.files && event.target.files[0]) {
+            setSelectedFile(event.target.files[0] as File);
+            setLoaded(0);
+        }
+        // this.setState({
+        //   selectedFile: event.target.files[0],
+        //   loaded: 0,
+        // })
+      }
+    
+    async function handleUpload(){
+        const data = new FormData()
+        if(selectedFile){
+            data.append('file', selectedFile as Blob, selectedFile.name as string)
+    
+        axios
+        .post(`${baseUrl.baseUrl}/upload`, data, {
+
+        //   .post(`${baseUrl.baseUrl}/users/updateImage/${userId}`, data, {
+            onUploadProgress: ProgressEvent => {
+                if(ProgressEvent.total){
+                    setLoaded((ProgressEvent.loaded / ProgressEvent.total) * 100)
+                }
+            //   this.setState({
+            //     loaded: (ProgressEvent.loaded / ProgressEvent.total) * 100,
+            //   })
+            },
+          })
+          .then(res => {
+            console.log(res.statusText)
+          })
+        }
     }
     // }
     return (
@@ -59,9 +103,14 @@ const Profile: FC<ProfileProps> = () => {
             </Typography>
             <Button variant="contained" component="label">
                 Upload
-                <input hidden accept="image/*" multiple type="file" onChange={updateImage}/>
+                <div className="App">
+                    <input type="file" name="" id="" onChange={handleselectedFile} />
+                    <button onClick={handleUpload}>Upload</button>
+                    <div> {loaded} %</div>
+                </div>
+                {/* <input hidden accept="image/*" multiple type="file" onChange={updateImage}/> */}
             </Button>
-            <img src = {selectedImage}></img>
+            {/* <img src = {selectedImage}></img> */}
         </div>
     )
 }
--- server/app.js
+++ server/app.js
@@ -4,15 +4,52 @@ const redis = require('redis');
 const client = redis.createClient();
 client.connect().then(() => {});
 const configRoutes = require('./routes');
+const path = require('path')
+// const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+const cors = require('cors')
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const cors = require('cors');
+// const cors = require('cors');
 app.use(cors());
+// app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+)
+app.use(cookieParser())
+app.use(fileUpload())
+
+app.use('/public', express.static(__dirname + '/public'))
+
+app.post('api/upload', (req, res, next) => {
+  let uploadFile = req.files.file
+  const fileName = req.files.file.name
+  uploadFile.mv(
+    `${__dirname}/public/files/${fileName}`,
+    function (err) {
+      if (err) {
+        return res.status(500).send(err)
+      }
+
+      res.json({
+        file: `public/${req.files.file.name}`,
+      })
+    },
+  )
+})
+
 
 configRoutes(app);
+
+
 
 const port = 3008;
 app.listen(port, async () => {
